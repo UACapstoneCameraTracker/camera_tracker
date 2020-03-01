5d1f349aa2ed0603f15b5965c568c11ea261051d
reup97 <puer@ualberta.ca>
2020-02-29 18:03:57 -0700

    update test cases

--- test/test_pixel_diff_detector.py
+++ test/test_pixel_diff_detector.py
@@ -2,8 +2,8 @@ import unittest
 import cv2
 from camera_tracker.predictors import PixelDifferenceDetector
 
-img1 = cv2.imread('img1.png')
-img2 = cv2.imread('img2.png')
+img1 = cv2.imread('tracking_img1.png')
+img2 = cv2.imread('tracking_img2.png')
 
 gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
@@ -11,11 +11,15 @@ gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 class DetectorTest(unittest.TestCase):
     def test_not_gray_scale(self):
         with self.assertRaises(Exception) as context:
-            detector = PixelDifferenceDetector()
+            detector = PixelDifferenceDetector(pixel_difference_threshold=10,
+                                                  structuring_kernel_shape=(20, 20),
+                                                  bbox_area_min=150)
             detector.predict(img1)
     
     def test_detect(self):
-        detector = PixelDifferenceDetector()
+        detector = PixelDifferenceDetector(pixel_difference_threshold=10,
+                                                  structuring_kernel_shape=(20, 20),
+                                                  bbox_area_min=150)
         ret = detector.predict(gray1)
         self.assertFalse(ret[0])
 
