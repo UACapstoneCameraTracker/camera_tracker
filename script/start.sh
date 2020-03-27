1041d677dd946eefe44eace3c16498fcb13d89d4
reup97 <puer@ualberta.ca>
2020-03-27 01:32:15 -0600

    add launch all script

---
+++ script/start.sh
@@ -0,0 +1,4 @@
+tmux \
+    new-session  'sudo pigpiod && source ~/tracking_system/venv/bin/activate && python ~/tracking_system/camera_tracker/app/app.py' \; \
+    split-window 'source ~/tracking_system/venv/bin/activate && python ~/tracking_system/CamServer/webapp/app.py' \; \
+    detach-client
